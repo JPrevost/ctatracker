d9da822eef2f843a3d5c0b2afb61b029805272e4
Jeremy Prevost <jerrocks@gmail.com>
2012-07-27 11:17:17 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ctatracker::Application.config.secret_token = '4300a2d68abfcaee44a659d6291ecce358775ec2923c7a5e408195183a2e8730206f5d0d10447ac28bfd70c51e356a04bfd81a973ef73020be9156acec134577'
