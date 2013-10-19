0556aa11f896acc2939d36fcf58588fbd9506f83
Jeremy Prevost <jerrocks@gmail.com>
2013-10-19 13:15:20 -0500

    Passing tests and spacing cleanup

--- spec/requests/bustime_pages_spec.rb
+++ spec/requests/bustime_pages_spec.rb
@@ -2,42 +2,36 @@ require 'spec_helper'
 
 describe "BustimePages" do
 
-	subject { page }
+  subject { page }
 
-	describe "Home page" do
-	
-		before { visit root_path }
-	
-		it { should have_selector('h2', text: 'Choose a route') }
-		
-		it { find_link('Choose a Route').visible?.should equal true }
-	
-		it { find_link('147 Outer Drive Express').visible?.should equal true }
+  before { visit root_path }
 
-		describe "clicking 147 bus" do
-			
-			before { click_link('147 Outer Drive Express') }
-			
-			it { find_link('North Bound').visible?.should equal true }
-			
-			describe "clicking North Bound" do
-			
-				before { click_link('North Bound') }	
-			
-				it { find_link('Michigan & Huron').visible?.should equal true }
-			
-				describe "Clicking Michigan & Huron" do
-					
-					before { click_link('Michigan & Huron') }
-					
-					it { should have_content('Refresh') }
-					
-				end
-			
-			end
-			
-		end
-		
-	end
-	
+  describe "Home page" do
+
+    it { should have_selector('h2', text: 'Choose a route') }
+
+    it { find_link('Choose a Route').visible?.should equal true }
+
+    it { find_link('147 Outer Drive Express').visible?.should equal true }
+
+    describe "clicking 147 bus" do
+
+      before { click_link('147 Outer Drive Express') }
+
+      it { find_link('Northbound').visible?.should equal true }
+
+      describe "clicking Northbound" do
+
+        before { click_link('Northbound') }
+
+        it { find_link('Michigan & Huron').visible?.should equal true }
+
+        describe "Clicking Michigan & Huron" do
+          before { click_link('Michigan & Huron') }
+
+          it { should have_content('Refresh') }
+        end
+      end
+    end
+  end
 end
