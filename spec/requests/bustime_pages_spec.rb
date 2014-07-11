bbc2ea93f288e5969155d8ad441f99dba7d14202
Jeremy Prevost <jerrocks@gmail.com>
2014-07-10 20:35:13 -0500

    Updates to rspec expect syntax
    - also updates some other deprecations

--- spec/requests/bustime_pages_spec.rb
+++ spec/requests/bustime_pages_spec.rb
@@ -1,73 +1,67 @@
 require 'spec_helper'
 
 describe "BustimePages", :vcr do
-
   subject { page }
-
   before { visit root_path }
 
   describe "Home page" do
-
     it "should have a breadcrumb" do
-      page.should have_content "Choose a Route"
+      expect(page).to have_content("Choose a Route")
     end
 
     it "should display links to various routes" do
-     page.should have_link('147 Outer Drive Express', :href => busdirections_path(147,'Outer Drive Express'))
-     page.should have_link('66 Chicago', :href => busdirections_path(66,'Chicago'))
+     expect(page).to have_link('147 Outer Drive Express', :href => busdirections_path(147,'Outer Drive Express'))
+     expect(page).to have_link('66 Chicago', :href => busdirections_path(66,'Chicago'))
     end
 
     describe "clicking 147 bus" do
-
       before { click_link('147 Outer Drive Express') }
 
       it "should have a breadcrumb" do
-        page.should have_content "Route Choose a Direction"
+        expect(page).to have_content "Route Choose a Direction"
       end
 
       it "should have direction links" do
-        page.should have_link('Northbound', :href => busstops_path(147,'Outer Drive Express', 'Northbound'))
-        page.should have_link('Southbound', :href => busstops_path(147,'Outer Drive Express', 'Southbound'))
+        expect(page).to have_link('Northbound', :href => busstops_path(147,'Outer Drive Express', 'Northbound'))
+        expect(page).to have_link('Southbound', :href => busstops_path(147,'Outer Drive Express', 'Southbound'))
       end
 
       it "should display route information" do
-        page.should have_content "147 Outer Drive Express"
+        expect(page).to have_content "147 Outer Drive Express"
       end
 
       describe "clicking Northbound" do
-
         before { click_link('Northbound') }
 
         it "should have a breadcrumb" do
-          page.should have_content "Route Direction Choose a Stop"
+          expect(page).to have_content "Route Direction Choose a Stop"
         end
 
         it "should list bus stops" do
-          page.should have_link('Michigan & Huron', :href => buspredictions_path(147,'Outer Drive Express', 'Northbound', 1125, 'Michigan & Huron'))
-          page.should have_link('Congress & Michigan', :href => buspredictions_path(147,'Outer Drive Express', 'Northbound', 4725, 'Congress & Michigan'))
+          expect(page).to have_link('Michigan & Huron', :href => buspredictions_path(147,'Outer Drive Express', 'Northbound', 1125, 'Michigan & Huron'))
+          expect(page).to have_link('Congress & Michigan', :href => buspredictions_path(147,'Outer Drive Express', 'Northbound', 4725, 'Congress & Michigan'))
         end
-
 
         describe "Clicking Michigan & Huron" do
           before { click_link('Michigan & Huron') }
 
           it "should have a breadcrumb" do
-            page.should have_content "Route Direction Stop Upcoming Buses"
+            expect(page).to have_content "Route Direction Stop Upcoming Buses"
           end
 
           it "should display route and stop information" do
-            page.should have_content "147 Outer Drive Express, Northbound, Michigan & Huron"
+            expect(page).to have_content "147 Outer Drive Express, Northbound, Michigan & Huron"
           end
 
           it "should display bus predictions" do
-            page.should have_content "2 Min (to Howard Station)"
-            page.should have_content "28 Min (to Howard Station)"
+            expect(page).to have_content "2 Min (to Howard Station)"
+            expect(page).to have_content "28 Min (to Howard Station)"
           end
 
           it { should have_content('Refresh') }
 
           it "should have more info links to bus detail" do
-            page.should have_link("More Info", :href => "#modal-4077")
+            expect(page).to have_link("More Info", :href => "#modal-4077")
           end
 
           describe 'recent stops' do
@@ -78,15 +72,15 @@ describe "BustimePages", :vcr do
             end
 
             it 'only remembers 6 recent stops and removes the least recently used' do
-              pending
+              skip 'write this test'
             end
 
             it 'only remembers unique recent stops' do
-              pending
+              skip 
             end
 
             it 'moves the most recent stop to the first position' do
-              pending
+              skip 
             end
           end
         end
@@ -94,3 +88,4 @@ describe "BustimePages", :vcr do
     end
   end
 end
+
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,6 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'rspec/autorun'
 require 'capybara'
 require 'coveralls'
 Coveralls.wear!
@@ -12,6 +11,7 @@ Coveralls.wear!
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
+  config.include Rails.application.routes.url_helpers
   # ## Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
