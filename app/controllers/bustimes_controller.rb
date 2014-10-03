f7b82870743f922e9b430451f2456417e9c5b02c
Jeremy Prevost <jerrocks@gmail.com>
2014-10-03 15:44:51 -0500

    controller and model cleanup

--- app/controllers/bustimes_controller.rb
+++ app/controllers/bustimes_controller.rb
@@ -1,43 +1,41 @@
 class BustimesController < ApplicationController
   def busroutes
-  	@busroutes = Bustime.new.busroutes
+    @busroutes = Bustime.new.busroutes
   end
 
   def busdirections
-  	@busdirections = Bustime.new.busdirections( params[:id] )
+    @busdirections = Bustime.new.busdirections(params[:id])
   end
 
   def busstops
-  	@busstops = Bustime.new.busstops( params[:id], params[:dir] )
+    @busstops = Bustime.new.busstops(params[:id], params[:dir])
   end
 
   def buspredictions
-    set_recent_stops_cookie(params[:id], params[:rtnm], params[:dir], params[:stopid], params[:stpnm])
-  	@buspredictions = Bustime.new.buspredictions( params[:id], params[:dir], params[:stopid] )
+    set_recent_stops_cookie(params[:id], params[:rtnm], params[:dir],
+                            params[:stopid], params[:stpnm])
+    @buspredictions = Bustime.new.buspredictions(params[:id], params[:dir],
+                                                 params[:stopid])
   end
 
   def set_recent_stops_cookie(id, rtnm, dir, stopid, stpnm)
     cookies[:recent_stops] = {
-      value: ActiveSupport::JSON.encode(append_cookie_value(id, rtnm, dir, stopid, stpnm)),
-      expires: 1.year.from_now
-    }
+      value: ActiveSupport::JSON.encode(
+        append_cookie_value(id, rtnm, dir, stopid, stpnm)),
+      expires: 1.year.from_now }
   end
 
   def read_recent_stops_cookie
-    begin
-      ActiveSupport::JSON.decode(cookies[:recent_stops])
-    rescue TypeError
-      []
-    end
+    ActiveSupport::JSON.decode(cookies[:recent_stops])
+  rescue TypeError
+    []
   end
 
   def append_cookie_value(id, rtnm, dir, stopid, stpnm)
     recent_stops = read_recent_stops_cookie
-    if recent_stops.count > 5
-      recent_stops.pop
-    end
-    stop = { "route" => id, "route_name" => rtnm, "dir" => dir,
-      "stopid" => stopid, "stpnm" => stpnm }
+    recent_stops.pop if recent_stops.count > 5
+    stop = { 'route' => id, 'route_name' => rtnm, 'dir' => dir,
+      'stopid' => stopid, 'stpnm' => stpnm }
     recent_stops.unshift(stop).uniq
   end
 end
--- app/models/bustime.rb
+++ app/models/bustime.rb
@@ -1,61 +1,67 @@
 class Bustime
-
   require 'nokogiri'
   require 'open-uri'
 
   def busroutes
-    #sample: http://www.ctabustracker.com/bustime/api/v1/getroutes?key=APIKEY
+    # sample: http://www.ctabustracker.com/bustime/api/v1/getroutes?key=APIKEY
 
-    apiurl = 'http://www.ctabustracker.com/bustime/api/v1/getroutes?key=' + (ENV['CTA_API_KEY'] || 'YOUR_CTA_API_KEY')
+    apiurl = 'http://www.ctabustracker.com/bustime/api/v1/getroutes?key=' +
+               (ENV['CTA_API_KEY'] || 'YOUR_CTA_API_KEY')
 
     doc = Nokogiri::XML(open(apiurl))
 
-    allroutes = doc.xpath('//route').map do |i|
-      {'rt' => i.xpath('rt').inner_text, 'rtnm' => i.xpath('rtnm').inner_text}
+    doc.xpath('//route').map do |i|
+      { 'rt' => i.xpath('rt').inner_text, 'rtnm' => i.xpath('rtnm').inner_text }
     end
   end
 
   def busdirections(rt)
-    #sample: http://www.ctabustracker.com/bustime/api/v1/getdirections?key=APIKEY&rt=147
+    # sample: http://www.ctabustracker.com/bustime/api/v1/getdirections?key=APIKEY&rt=147
 
-    doc = Nokogiri::XML(open('http://www.ctabustracker.com/bustime/api/v1/getdirections?key=' + 
-                        (ENV['CTA_API_KEY'] || 'YOUR_CTA_API_KEY') + '&rt=' + rt.to_s ))
+    doc = Nokogiri::XML(
+            open(
+              'http://www.ctabustracker.com/bustime/api/v1/getdirections?key=' +
+                (ENV['CTA_API_KEY'] || 'YOUR_CTA_API_KEY') + '&rt=' + rt.to_s))
 
-    directions = doc.xpath('//dir').map do |i|
-      {'dir' => i.inner_text }
+    doc.xpath('//dir').map do |i|
+      { 'dir' => i.inner_text }
     end
   end
 
-  def busstops(rt,dir)
-    #sample http://www.ctabustracker.com/bustime/api/v1/getstops?key=APIKEY&rt=147&dir=North%20Bound
+  def busstops(rt, dir)
+    # sample http://www.ctabustracker.com/bustime/api/v1/getstops?key=APIKEY&rt=147&dir=North%20Bound
 
-    doc = Nokogiri::XML(open('http://www.ctabustracker.com/bustime/api/v1/getstops?key=' + 
-                              (ENV['CTA_API_KEY'] || 'YOUR_CTA_API_KEY') + '&rt=' + rt.to_s + '&dir=' + URI::encode(dir)))
+    doc = Nokogiri::XML(
+            open('http://www.ctabustracker.com/bustime/api/v1/getstops?key=' +
+                  (ENV['CTA_API_KEY'] || 'YOUR_CTA_API_KEY') + '&rt=' +
+                    rt.to_s + '&dir=' + URI.encode(dir)))
 
-    stops = doc.xpath('//stop').map do |i|
-      {'stpid' => i.xpath('stpid').inner_text, 'stpnm' => i.xpath('stpnm').inner_text}
+    doc.xpath('//stop').map do |i|
+      { 'stpid' => i.xpath('stpid').inner_text,
+        'stpnm' => i.xpath('stpnm').inner_text }
     end
   end
 
-  def buspredictions(rt,dir,stpid)
-    #sample http://www.ctabustracker.com/bustime/api/v1/getpredictions?key=APIKEY&rt=147&dir=North%20Bound&stpid=1125
+  def buspredictions(rt, dir, stpid)
+    # sample http://www.ctabustracker.com/bustime/api/v1/getpredictions?key=APIKEY&rt=147&dir=North%20Bound&stpid=1125
 
-    doc = Nokogiri::XML(open('http://www.ctabustracker.com/bustime/api/v1/getpredictions?key=' + 
-                              (ENV['CTA_API_KEY'] || 'YOUR_CTA_API_KEY') + '&rt=' + rt.to_s + '&dir=' + URI::encode(dir) + '&stpid=' + stpid.to_s))
+    doc = Nokogiri::XML(
+      open('http://www.ctabustracker.com/bustime/api/v1/getpredictions?key=' +
+            (ENV['CTA_API_KEY'] || 'YOUR_CTA_API_KEY') + '&rt=' + rt.to_s +
+              '&dir=' + URI.encode(dir) + '&stpid=' + stpid.to_s))
 
-    predictions = doc.xpath('//prd').map do |i|
-      {'tmstmp' => i.xpath('tmstmp').inner_text,
+    parse_prediction(doc)
+  end
+
+  def parse_prediction(doc)
+    doc.xpath('//prd').map do |i|
+      { 'tmstmp' => i.xpath('tmstmp').inner_text,
         'stpnm' => i.xpath('stpnm').inner_text,
         'vid' => i.xpath('vid').inner_text,
         'rt' => i.xpath('rt').inner_text,
         'rtdir' => i.xpath('rtdir').inner_text,
         'des' => i.xpath('des').inner_text,
-        'prdtm' => i.xpath('prdtm').inner_text,
-      }
+        'prdtm' => i.xpath('prdtm').inner_text }
     end
-  end
-
-  def persisted?
-    false
   end
 end
