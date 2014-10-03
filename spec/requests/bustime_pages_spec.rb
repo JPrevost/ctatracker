f6c76f7f10775aeba28aa372ca3aaf0a5daa154a
Jeremy Prevost <jerrocks@gmail.com>
2014-10-03 13:50:45 -0500

    Finish pending tests
    closes #7

--- spec/requests/bustime_pages_spec.rb
+++ spec/requests/bustime_pages_spec.rb
@@ -63,27 +63,71 @@ describe "BustimePages", :vcr do
           it "should have more info links to bus detail" do
             expect(page).to have_link("More Info", :href => "#modal-4077")
           end
-
-          describe 'recent stops' do
-            it 'remembers recently used stops' do
-              visit root_path
-              expect(page).to have_content("Recent Stops")
-              expect(page).to have_link("147 Outer Drive Express, Northbound, Michigan & Huron")
-            end
-
-            it 'only remembers 6 recent stops and removes the least recently used' do
-              skip 'write this test'
-            end
-
-            it 'only remembers unique recent stops' do
-              skip 
-            end
-
-            it 'moves the most recent stop to the first position' do
-              skip 
-            end
-          end
         end
+      end
+    end
+  end
+
+  describe 'recent stops' do
+    it 'contains no recent stops initially' do
+      visit root_path
+      expect(page).to_not have_content('Recent Stops')
+    end
+
+    it 'remembers recently used stops' do
+      visit '/bustimes/147/Outer%20Drive%20Express/Northbound/1125/Michigan%20&%20Huron'
+      visit root_path
+      expect(page).to have_content('Recent Stops')
+      expect(page).to have_link('147 Outer Drive Express, Northbound, Michigan & Huron')
+    end
+
+    it 'only remembers 6 recent stops and removes the least recently used' do
+      visit '/bustimes/147/Outer%20Drive%20Express/Northbound/1125/Michigan%20&%20Huron'
+      visit '/bustimes/147/Outer%20Drive%20Express/Southbound/1041/Sheridan%20&%20Berwyn%20'
+      visit '/bustimes/66/Chicago/Westbound/596/Fairbanks%20&%20Chicago'
+      visit '/bustimes/66/Chicago/Eastbound/530/Chicago%20&%20Pulaski'
+      visit '/bustimes/146/Inner%20Drive%2FMichigan%20Express/Northbound/4877/Solidarity%20Dr%20&%20Planetarium'
+      visit '/bustimes/146/Inner%20Drive%2FMichigan%20Express/Southbound/4846/Foster%20&%20Marine%20Drive'
+      visit root_path
+      expect(page).to have_content('Recent Stops')
+      expect(page).to have_link('147 Outer Drive Express, Northbound, Michigan & Huron')
+      expect(page).to have_link('147 Outer Drive Express, Southbound, Sheridan & Berwyn')
+      expect(page).to have_link('66 Chicago, Westbound, Fairbanks & Chicago')
+      expect(page).to have_link('66 Chicago, Eastbound, Chicago & Pulaski')
+      expect(page).to have_link('146 Inner Drive/Michigan Express, Northbound, Solidarity Dr & Planetarium')
+      expect(page).to have_link('146 Inner Drive/Michigan Express, Southbound, Foster & Marine Drive')
+
+      visit '/bustimes/147/Outer%20Drive%20Express/Northbound/1123/Michigan%20&%20Grand'
+      visit root_path
+      expect(page).to_not have_link('147 Outer Drive Express, Northbound, Michigan & Huron')
+      expect(page).to have_link('147 Outer Drive Express, Northbound, Michigan & Grand')
+    end
+
+    it 'only remembers unique recent stops' do
+      visit '/bustimes/66/Chicago/Westbound/596/Fairbanks%20&%20Chicago'
+      visit '/bustimes/66/Chicago/Westbound/596/Fairbanks%20&%20Chicago'
+      visit root_path
+      expect(page).to have_link('66 Chicago, Westbound, Fairbanks & Chicago',
+                                count: 1)
+    end
+
+    it 'moves the most recent stop to the first position' do
+      visit '/bustimes/66/Chicago/Westbound/596/Fairbanks%20&%20Chicago'
+      visit '/bustimes/147/Outer%20Drive%20Express/Northbound/1125/Michigan%20&%20Huron'
+      visit root_path
+
+      within '#recent_stops' do
+        expect(page).to have_selector('li:nth-child(1)', text: 'Huron')
+        expect(page).to have_selector('li:nth-child(2)', text: 'Fairbanks')
+      end
+
+      visit '/bustimes/66/Chicago/Eastbound/530/Chicago%20&%20Pulaski'
+      visit root_path
+
+      within '#recent_stops' do
+        expect(page).to have_selector('li:nth-child(1)', text: 'Pulaski')
+        expect(page).to have_selector('li:nth-child(2)', text: 'Huron')
+        expect(page).to have_selector('li:nth-child(3)', text: 'Fairbanks')
       end
     end
   end
